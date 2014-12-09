2f3a5c73816c0a0c9611aee43685aae56e721021
timmycheng <timmycheng@foxmail.com>
2014-12-09 08:56:27 +0800

    search functon imporve
    type key word to find ip information

--- source/looks_up.php
+++ source/looks_up.php
@@ -17,33 +17,40 @@ echo "数据库连接成功！";
 
 echo '<br>';
 
-if (!empty($ip) && count(explode('.', $ip))==4) {
-
-$ip_dec=ipstr2dec($ip);//计算IP十进制值
-
-$existsql = "SELECT * from pro1_ip_looks_up where ip_dec_begin<=$ip_dec and ip_dec_end>=$ip_dec";
-$exist = mysql_query($existsql);  //执行SQL
-$row_num=mysql_num_rows($exist);
-
-echo '共'.$row_num.'条记录！<br><hr>';
-
-
-while($row = mysql_fetch_array($exist))
+if (!empty($ip) && count(explode('.', $ip))==4) 
 {
-  $row['ip_subnet']==0?$sub_show=null:$sub_show="/".$row['ip_subnet'];
-  echo "地址： ".$row['ip_name'] . $sub_show;
-  echo "<br>";
-  echo "分类： ".$row['cate'];
-  echo "<br>";
-  echo "描述： ".$row['desc'];
-  echo "<br>";
-  echo "<hr>";
-
-}
-
-}elseif(empty($ip)){
+	$ip_dec=ipstr2dec($ip);//计算IP十进制值
+	$existsql = "select * from pro1_ip_looks_up where ip_dec_begin<=$ip_dec and ip_dec_end>=$ip_dec";
+}elseif(empty($ip))
+{
 	echo "请输入要查找的IP！";
+}elseif (count(explode('.', $ip))) 
+{
+	//IP不全
+	$existsql = "select * from pro1_ip_looks_up where ip_name like '%$ip%' or `desc` like '%$ip%'";
 }
+
+if (!empty($existsql)) {
+	$exist = mysql_query($existsql);  //执行SQL
+	$row_num=mysql_num_rows($exist);
+
+	echo '共'.$row_num.'条记录！<br><hr>';
+
+	while($row = mysql_fetch_array($exist))
+	{
+	  $row['ip_subnet']==0?$sub_show=null:$sub_show="/".$row['ip_subnet'];
+	  echo "地址： ".$row['ip_name'] . $sub_show;
+	  echo "<br>";
+	  echo "分类： ".$row['cate'];
+	  echo "<br>";
+	  echo "描述： ".$row['desc'];
+	  echo "<br>";
+	  echo "<hr>";
+
+	}
+}
+
+
 
 
 ?>
